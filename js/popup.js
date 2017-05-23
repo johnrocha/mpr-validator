d32cf7a2fe517a35ce1f88c8a993801261b110e1
johnrocha <johnerickrocha@gmail.com>
2017-05-23 13:16:10 -0300

    push

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    console.log("JQUERY IS READY!");
+    $("#modal-open-button").click(function (e) {
+        console.log('$("#modal-open-button").click');
+        openPopup();
+    });
+    $("#modal-close-button").click(function (e) {
+        console.log('$("#modal-close-button").click');
+        closePopup();
+    });
+    $("#modal-closee-button").click(function (e) {
+        console.log('$("#modal-close-button").click');
+        closePopup();
+    });
+});
+function openPopup() {
+    $("#modal-open-button").prop("disabled", true);
+    $("#popup-content").fadeIn();
+}
+function closePopup() {
+    $("#modal-open-button").prop("disabled", false);
+    $("#popup-content").fadeOut();
+}
